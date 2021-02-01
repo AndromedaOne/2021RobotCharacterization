9415e7fc5e9a6e5bc14fc82038fdccbf9a58f896
SPD3 <sean@ayerdoyles.org>
2021-01-31 22:29:43 -0500

    Last run of the characterization.

--- robotconfig.py
+++ robotconfig.py
@@ -34,7 +34,7 @@
     # If doing drive test, treat this as the left side of the drivetrain
     "encoderInverted": False,
     # Only if you are doing drive (set to False if not needed)
-    "rightEncoderInverted": False,
+    "rightEncoderInverted": True,
     # ** The following is only if you are using a gyro for the DriveTrain test**
     # Gyro type (one of "NavX", "Pigeon", "ADXRS450", "AnalogGyro", or "None")
     "gyroType": "NavX",
